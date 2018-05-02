97de5df4485f0de8e4e34c9297671567037f1016
bodie101 <keysbw@g.toiohomai.ac.nz>
2018-05-02 12:37:01 +1200

    inital commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace carClass2
+{
+    class Car
+    {
+        public string Make {get; set;}
+        public string Color {get; set;}
+        public string Gearbox {get; set;}
+
+        public string DescribeCar() {
+            return $"This car is a {Make} and is {Color}, the transmission is a {Gearbox}.";
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter new car below");
+            Console.Write("Make: "); 
+            var make = Console.ReadLine();
+            Console.Write("Color: ");
+            var color = Console.ReadLine();
+            Console.Write("Gearbox; ");
+            var gearbox = Console.ReadLine();
+
+            Console.WriteLine();
+
+            var car1 = new Car(); 
+            car1.Make = make; 
+            car1.Color = color; 
+            car1.Gearbox = gearbox; 
+            Console.WriteLine(car1.DescribeCar()); 
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
